b4a2ed543ccc6026276a47b7f7c28744ababa418
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-02-07 16:47:40 +0100

    sync 19_01_2024 && ecu_containers_poc

---
+++ projekt/kod/ECU_containers_poc/gw/setup_gw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+nbuses=3
+
+echo "Setting up can gateway..."
+for i in $(seq 0 $nbuses)
+do
+    ip link add dev vcan$i type vcan
+    ip link set dev vcan$i up
+done
+
+echo "Setup done"
+
+/bin/bash
