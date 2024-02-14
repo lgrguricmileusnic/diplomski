334e51ff29a17ea87934461d642fd7c278b0134c
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-02-14 17:50:26 +0100

    biljeske sync

--- projekt/kod/ECU_containers_poc/ecu/setup_cangw.sh
+++ projekt/kod/ECU_containers_poc/ecu/setup_cangw.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cangw -A -s vcan0 -d ecu_vxcan -X -e
-cangw -A -s ecu_vxcan -d vcan0 -X -e
+cangw -A -s vcan0 -d ecu_vxcan -X -e -i
+cangw -A -s ecu_vxcan -d vcan0 -X -e -i
+
+
+# cangw -A -s vcan0 -d ecu_vxcan -X -e
+# cangw -A -s ecu_vxcan -d vcan0 -X -e 
--- projekt/kod/ECU_containers_poc/gw/setup_cangw.sh
+++ projekt/kod/ECU_containers_poc/gw/setup_cangw.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cangw -A -s gw_vxcan1 -d vcan1 -X -e
-cangw -A -s vcan1 -d gw_vxcan1 -X -e
-cangw -A -s vcan1 -d gw_vxcan2 -X -e
-cangw -A -s gw_vxcan2 -d vcan1 -X -e
+# cangw -A -s gw_vxcan1 -d vcan1 -X -e
+# cangw -A -s vcan1 -d gw_vxcan1 -X -e
+# cangw -A -s vcan1 -d gw_vxcan2 -X -e
+# cangw -A -s gw_vxcan2 -d vcan1 -X -e
+
+cangw -A -s gw_vxcan1 -d vcan1 -X -e -i
+# cangw -A -s vcan1 -d gw_vxcan1 -X -e -i
+# cangw -A -s vcan1 -d gw_vxcan2 -X -e -i
+# cangw -A -s gw_vxcan2 -d vcan1 -X -e -i
