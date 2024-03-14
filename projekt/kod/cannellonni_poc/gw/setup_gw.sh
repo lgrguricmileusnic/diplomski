7202f87558d843fa28cc132feb52b828b167db96
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-03-14 16:53:07 +0100

    biljeske sync && rad

--- projekt/kod/cannellonni_poc/gw/setup_gw.sh
+++ projekt/kod/cannellonni_poc/gw/setup_gw.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-ip link add dev vcan0 type vcan
-ip link set dev vcan0 up
+nbuses=3
 
-echo "Set up vcan0"
+echo "Setting up can gateway..."
+for i in $(seq 1 $nbuses)
+do
+    ip link add dev vcan$i type vcan
+    ip link set dev vcan$i up
+done
+
+cannelloni/cannelloni -I vcan1 -C s -R ecu1 -r 10001 -l 10001 -f
+cannelloni/cannelloni -I vcan2 -C s -R ecu2 -r 10002 -l 10002 -f
+
+echo "Setup done"
 
 /bin/bash
