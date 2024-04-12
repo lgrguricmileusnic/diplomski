754ba18b3d04cdce30160e38d147a93387ecc0b6
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-04-12 23:45:05 +0200

    biljeske i kod sync

--- rad/kod/ecu-template/main.py
+++ rad/kod/ecu-template/main.py
@@ -1,17 +1,17 @@
 import argparse
-import ecu.callbacks.can_raw as can_raw
-from ecu import ecu
+import ecu_template.callbacks.can_raw as can_raw
+from ecu_template.ecu import ECU
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("interface", default="vcan0", type=str, nargs="?")
     args = argparser.parse_args()
 
-    ecu1 = ecu.CANController(args.interface)
-    ecu1.raw_can_handler.add_callback_for_id(can_raw.echo, 0x123)
+    ecu1 = ECU(interface="vcan0")
+    ecu1.can_controller.add_callback_for_id(can_raw.echo, 0x123)
     ecu1.start()
     try:
-        while(True):
+        while True:
             pass
     except KeyboardInterrupt:
-        ecu.stop()
+        ecu1.stop()
