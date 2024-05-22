7f8a87a82bf4115525d112fc227964f9495b79ad
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-05-22 19:46:57 +0200

    Biljeske i kod sync

--- rad/kod/agl-ic-compose/gui/start.sh
+++ rad/kod/agl-ic-compose/gui/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker run -it --rm -d --net=host -v $HOME/kuksaval.config:/config  -p 127.0.0.1:8090:8090 -e LOG_LEVEL=ALL ghcr.io/eclipse/kuksa.val/kuksa-val:master
+flutter run
 
-
-
+/bin/bash
